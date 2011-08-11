277505b2b410ceb7e8512ade21bad1d00a98ab88
John Chilton <chilton@gust.msi.umn.edu>
2011-08-11 14:20:20 -0500

    Small improvements/fixes to the resource report query.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -8,15 +8,15 @@ class Resource < ReadOnlyModel
   
   scope :usage_report, select("resources.id, count(distinct users.id) as num_users, count(distinct groups.gid) as num_groups").
                        where("event.operation = 'OUT'").
-                       order("resources.name ASC").
                        joins(:executables => { :events => { :process_user => :group} }).
-                       group("resources.name")
+                       group("resources.id")
 
   scope:report, select("resources.id, resources.name, num_users, num_groups, fy10, fy11, fy12, fy13").
                 joins("inner join (#{Purchase.resource_summary.to_sql}) rs on rs.rid = resources.id                                            
-                        inner join (#{Resource.usage_report.to_aliased_sql('ir')}) ur on ur.id = resources.id")
+                       inner join (#{Resource.usage_report.to_aliased_sql('ir')}) ur on ur.id = resources.id").
+                order("resources.name")
 
-
+                
   def self.purchases_for(rid)
     where("id = ?", rid).includes(:purchases).first.purchases
   end
