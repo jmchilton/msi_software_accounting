830ec13dbf64535d052f7819a285eaf5e87b86f1
John Chilton <jmchilton@gmail.com>
2011-11-13 23:41:25 -0600

    Small code tweak.

--- app/controllers/resources_report_controller.rb
+++ app/controllers/resources_report_controller.rb
@@ -1,14 +1,14 @@
 class ResourcesReportController < ReportController
   FIELDS = [id_field,
-                     resource_name_field,
-                     num_users_field,
-                     num_groups_field,
-                     fy_10_field,
-                     fy_11_field,
-                     fy_12_field,
-                     fy_13_field,
-                     link_field(:link_proc => "resource_path")
-                     ]
+            resource_name_field,
+            num_users_field,
+            num_groups_field,
+            fy_10_field,
+            fy_11_field,
+            fy_12_field,
+            fy_13_field,
+            link_field(:link_proc => "resource_path")
+            ]
 
   protected
 
